c3acc66ea23473aa70bbfeac5a686cdd0f917017
舵主 陈 <617316241@qq.com>
2019-03-20 14:33:52 +0800

    增加思路。

--- Chapter01/festival03/_1_3_4_Parentheses.java
+++ Chapter01/festival03/_1_3_4_Parentheses.java
@@ -2,6 +2,14 @@ package festival03;
 
 import edu.princeton.cs.algs4.StdIn;
 
+/**
+ * 思路：
+ * 将括号依次录入字符串栈中，当出现右括号时，比较右括号和前一个入栈的左括号是否配对。
+ * 若配对，输出配对的是哪一种括号；接着继续向栈中录入括号。
+ * 若不配对，输出False.
+ * @author Administrator
+ *
+ */
 public class _1_3_4_Parentheses {
 
 	public static void main(String[] args) {
