4dde04e737fefd72004a3f00f3bd6aae5339d8c5
舵主 陈 <617316241@qq.com>
2019-03-21 23:14:19 +0800

    链表类

---
+++ Chapter01/festival03/Node.java
@@ -0,0 +1,15 @@
+package festival03;
+
+public class Node<Item> {
+	Item item;
+	Node<Item> next;
+	
+	Node(Item item){
+		this.item = item;
+		this.next = null;
+	}
+	Node(Item item,Node next){
+		this.item = item;
+		this.next = next;
+	}
+}
