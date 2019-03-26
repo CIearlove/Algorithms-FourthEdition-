3400c05ff75c0152956616e222ad0d1ecff48b24
舵主 陈 <617316241@qq.com>
2019-03-26 11:50:45 +0800

    修改DoubleNode

--- Chapter01/festival03/DoubleNode.java
+++ Chapter01/festival03/DoubleNode.java
@@ -1,16 +1,17 @@
 package festival03;
 
 public class DoubleNode<Item> {
+	
 	Item item;
-	Node<Item> pre;
-	Node<Item> next;
+	DoubleNode<Item> pre;
+	DoubleNode<Item> next;
 	
 	DoubleNode(Item item){
 		this.item = item;
 		this.pre = null;
 		this.next = null;
 	}
-	DoubleNode(Item item,Node<Item> pre,Node<Item> next){
+	DoubleNode(Item item,DoubleNode<Item> pre,DoubleNode<Item> next){
 		this.item = item;
 		this.pre = pre;
 		this.next = next;
