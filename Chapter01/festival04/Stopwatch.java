963a20c2a3ed3e528e13e9d0189f07918005b3d2
舵主 陈 <617316241@qq.com>
2019-03-28 16:04:45 +0800

    一种表示计时器的抽象数据类型。

---
+++ Chapter01/festival04/Stopwatch.java
@@ -0,0 +1,18 @@
+package festival04;
+/**
+ * 一种表示计时器的抽象数据类型
+ * @author Administrator
+ *
+ */
+public class Stopwatch {
+	private final long start;
+	//创建一个计时器
+	public Stopwatch(){
+		start = System.currentTimeMillis();
+	}
+	//返回对象创建以来所经过的时间
+	public double elapsedTime(){
+		long now = System.currentTimeMillis();
+		return (now - start)/1000.0;
+	}
+}
